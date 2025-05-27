201213c8ec7e8d74c89c591c44d81b8553635fcc
Ken <ahmadsoud63@gmail.com>
2025-05-27 10:52:38 +0300

    add project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    project_manager = models.ForeignKey(
+        User, 
+        related_name='managed_projects',
+        on_delete=models.CASCADE
+    )
+    
+    participants = models.ManyToManyField(
+        User, 
+        related_name='participating_projects',
+        blank=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
