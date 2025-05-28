59c87890524a6fcf231761c07eb27ceaded14941
Ken <ahmadsoud63@gmail.com>
2025-05-28 16:55:53 +0300

    add project serializer

---
+++ projects/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+    project_manager = serializers.PrimaryKeyRelatedField(read_only=True)
+    
+    class Meta:
+        model = Project
+        fields = ['id', 'project_manager', 'name', 'description', 'created', 'updated']
+        read_only_fields = ['project_manager', 'created', 'updated']
